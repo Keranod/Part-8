512375dc78f8d229f129c0bc492067cafa1df438
Keranod <konrad.konkel@wp.pl>
2023-11-26 16:19:46 +0000

    8.6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -115,6 +116,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+        title: String!
+        author: String!
+        published: Int!
+        genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -150,12 +160,34 @@ const resolvers = {
                     booksCount++
                 }
             })
-            return {
+
+            const authorObject = {
                 name: author.name,
                 bookCount: booksCount
             }
+
+            if (!author.born) {
+                authorObject.born = null
+            } else {
+                authorObject.born = author.born
+            }
+
+            return authorObject
         })
         return allAuthors
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+        // checks above
+        if (!authors.find(author => author.name === args.author)) {
+            const author = { name: args.author, id: uuid() }
+            authors = authors.concat(author)
+        }
+
+        const book = { ...args, id: uuid() }
+        books = books.concat(book)
+        return book
     }
   }
 }
