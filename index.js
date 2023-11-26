929fc54b284deec21ac747b634da5161cb7ee60e
Keranod <konrad.konkel@wp.pl>
2023-11-26 15:08:58 +0000

    8.5

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -121,7 +121,17 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+        if (!args.author) {
+            return books
+        }
+        
+        const author = args.author
+
+        const booksByAuthor = books.filter(book => book.author === author)
+
+        return booksByAuthor
+    },
     allAuthors: () => {
         const allAuthors = authors.map(author => {
             let booksCount = 0
