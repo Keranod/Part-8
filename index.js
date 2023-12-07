bf2c33258bc639ccf26a37246797e5ed1d8ed076
keranod <konrad.konkel@wp.pl>
2023-12-07 09:46:57 +0000

    port 80 for aws

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ const server = new ApolloServer({
 })
 
 startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: 80 },
   context: async ({ req, res }) => {
     const auth = req ? req.headers.authorization : null
     if (auth && auth.startsWith('Bearer ')) {
