05d2516a559a7bd9c4242bfcb3110411644f1e01
Konrad Konkel <konrad.konkel@wp.pl>
2023-12-07 10:47:19 +0000

    8.17 take 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { useApolloClient } from '@apollo/client'
 import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
 const App = () => {
+  const [token, setToken] = useState(null)
   const [page, setPage] = useState('authors')
+  const client = useApolloClient()
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -2,17 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import {
-    ApolloClient,
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
   
-    ApolloProvider,
-    InMemoryCache,
-  } from '@apollo/client'
-  
-  const client = new ApolloClient({
-    uri: 'http://localhost:4000',
-    cache: new InMemoryCache(),
-  })
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('library-user-token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    }
+  }
+})
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+})
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+
+  link: authLink.concat(httpLink)
+})
 
   ReactDOM.createRoot(document.getElementById('root')).render(
 
